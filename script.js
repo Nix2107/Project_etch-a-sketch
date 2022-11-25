2d311a1930d43388c0205792cbabf76d39a91fa2
Nix2107 <15808234+Nix2107@users.noreply.github.com>
2022-11-25 10:59:22 +0300

    made the grid size selection work. Still blocked on color selection.

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ let sketchpad = document.querySelector ('#sketch-container')
 
 function generateGrid(unitsNum = prompt ('Choose the resolution of your sketchpad')) 
 {
-    sketchpad.setAttribute ('style', `grid-template-columns: repeat (${unitsNum}, 1fr)`);
-    sketchpad.setAttribute ('style', `grid-template-rows: repeat (${unitsNum}, 1fr)`);
+    sketchpad.setAttribute ('style', `grid-template-columns: repeat(${unitsNum}, 1fr)`,
+                            `grid-template-rows: repeat(${unitsNum}, 1fr)`);
 
     for (let index = 0; index < unitsNum * unitsNum; index++) 
     {
         let gridUnit = document.createElement ('div');
         gridUnit.setAttribute ('class', 'drawing-grid');
+        gridUnit.addEventListener ('mouseover', () => {setcolorMode()});
         sketchpad.appendChild (gridUnit);
-    }    
+    }  
+
 }
 
 let startSketch = document.querySelector ('#start-sketch');
 startSketch.addEventListener ('click', () => {generateGrid();})
+
+
+
