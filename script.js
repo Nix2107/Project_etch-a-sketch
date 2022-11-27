d0e3bac57a9ceafd9132d1ead916f4d1776f6d2e
Nix2107 <15808234+Nix2107@users.noreply.github.com>
2022-11-27 20:52:49 +0300

    all the logic for the web app is ready. next step is styling the ui

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 let sketchpad = document.querySelector ('#sketch-container')
-let drawingColor = 'grey'
+let drawingColor
+let rainbowMode = 'inactive'
+let colorMode = 'inactive'
+let mouseState = 'up'
+
+let blackColor = document.querySelector ('#black');
+blackColor.addEventListener ('click', () => {colorMode = 'active';
+                                            updateColor('black');});
+
+let greenColor = document.querySelector ('#green');
+greenColor.addEventListener ('click', () => {colorMode = 'active';
+                                            rainbowMode = 'inactive';
+                                            updateColor('green');});
+
+let blueColor = document.querySelector ('#blue');
+blueColor.addEventListener ('click', () => {colorMode = 'active';
+                                            rainbowMode = 'inactive';
+                                            updateColor('blue');});
+
+let redColor = document.querySelector ('#red');
+redColor.addEventListener ('click', () => {colorMode = 'active';
+                                            rainbowMode = 'inactive';
+                                            updateColor('red');});
+
+let rainbowBtn = document.querySelector ('#rainbow');
+rainbowBtn.addEventListener ('click', () => {rainbowMode = 'active';
+                                            colorMode = 'inactive';
+                                            updateColor(rainbow());});
+
+let startSketch = document.querySelector ('#start-sketch');
+startSketch.addEventListener ('click', () => {generateGrid();})
 
 function updateColor(color) 
 {
     drawingColor = color  
 }
-
-let blackColor = document.querySelector ('#black');
-blackColor.addEventListener ('click', () => {updateColor('black');});
-
-let greenColor = document.querySelector ('#green');
-greenColor.addEventListener ('click', () => {updateColor('green');});
-
-let blueColor = document.querySelector ('#blue');
-blueColor.addEventListener ('click', () => {updateColor('blue');});
-
-let redColor = document.querySelector ('#red');
-redColor.addEventListener ('click', () => {updateColor('red');});
-
-let rainbowBtn = document.querySelector ('#rainbow');
-rainbowBtn.addEventListener ('click', () => {updateColor(rainbow());});
-
-let startSketch = document.querySelector ('#start-sketch');
-startSketch.addEventListener ('click', () => {generateGrid();})
 
 function generateGrid(unitsNum = prompt ('Choose the resolution of your sketchpad')) 
 {
@@ -33,13 +45,22 @@ function generateGrid(unitsNum = prompt ('Choose the resolution of your sketchpa
     {
         let gridUnit = document.createElement ('div');
         gridUnit.setAttribute ('class', 'drawing-grid');
-        gridUnit.addEventListener ('mousedown', (e) => {e.target.style.backgroundColor = draw()});
-        gridUnit.addEventListener ('mouseover', (e) => {e.target.style.backgroundColor = draw ()});
+
+        gridUnit.addEventListener ('mousedown', () => { mouseState = 'down'});
+        gridUnit.addEventListener ('mouseup', () => {mouseState = 'up'});
+
+        gridUnit.addEventListener ('mouseover', (e) => { 
+            if (mouseState === 'down') {
+            if (colorMode === 'active')
+            {e.target.style.backgroundColor = `${drawingColor}`} else if (rainbowMode === 'active')
+            {e.target.style.backgroundColor = rainbow()}}});
+
         sketchpad.appendChild (gridUnit);
     }         
+    
 }
 
-function draw() 
+function rainbow() 
 {
     let randomRed = Math.floor(Math.random()*256);
     let randomGreen = Math.floor(Math.random()*256);
